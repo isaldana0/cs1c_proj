f75695d1d776b9d6d30cc81f6be3a1b8f6dd68bb
Gerald Judianto <gjudianto0@saddleback.edu>
2023-05-18 21:34:46 -0700

    added main window after login

--- bulkClub/mainwindow.cpp
+++ bulkClub/mainwindow.cpp
@@ -15,3 +15,39 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_searchSalesReport_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_salesReportMemberTypeDisplay_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_itemSold_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_totalRevenueTax_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_memberShoppingDataSearch_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_memberType_clicked()
+{
+
+}
+
